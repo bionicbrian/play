e370496135612b4c4886277f27eae36a2e5c2a0d
Brian Moore <bmoore@book.com>
2013-12-25 14:10:20 -0500

    Much progress with project-specific middleware
    * Mounted projects now use their own stylus middleware with ability to
      override main app stylesheet
    * Minor updates to layout.jade to show project name and nav if present
    
    Still need:
    * Better way to include main stylesheet. Better to do this in the main
      js file than in the stylesheet - "../../../____" is bad
    * Collapsable navigation

--- app.js
+++ app.js
@@ -1,37 +1,46 @@
 "use strict";
 
 var express = require("express");
+var stylus  = require("stylus");
 var http    = require("http");
 var path    = require("path");
 var fs      = require("fs");
 var app     = express();
 
-var bimodal = require("./lib/bimodal");
-var projects = fs.readdirSync("./lib");
+// Projects
+var dots = require("./projects/dots");
 
-app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(express.cookieParser('your secret here'));
-app.use(express.session());
-app.use(app.router);
-app.use(require('stylus').middleware(__dirname + '/public'));
-app.use(express.static(path.join(__dirname, 'public')));
-
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
-
-app.use(bimodal);
-
-app.get("/", function (req, res) {
-    res.render("index", { hideNav: true, projects: projects });
+var dirContents = fs.readdirSync("./projects");
+var projects = dirContents.filter(function (dir) {
+    return dir[0] !== ".";
 });
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+app.set("port", process.env.PORT || 3000);
+app.set("views", __dirname + "/views");
+app.set("view engine", "jade");
+
+app.use(express.favicon());
+app.use(express.logger("dev"));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.cookieParser("teo and tinker"));
+app.use(express.session());
+app.use(app.router);
+
+// Use project specific routes before stylus and static middleware
+app.use(dots);
+
+app.use(stylus.middleware(__dirname + "/public"));
+app.use(express.static(path.join(__dirname, "public")));
+
+if ("development" === app.get("env")) {
+    app.use(express.errorHandler());
+}
+
+app.get("/", function (req, res) {
+    res.render("index", { projects: projects });
+});
+
+http.createServer(app).listen(app.get("port"), function () {
+    console.log("Express server listening on port " + app.get("port"));
 });
---
+++ projects/dots/index.js
@@ -0,0 +1,16 @@
+"use strict";
+
+var path    = require("path");
+var stylus  = require("stylus");
+var express = require("express");
+var app     = express();
+
+app.set("views", __dirname);
+app.use(stylus.middleware(__dirname + "/public"));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/dots", function (req, res) {
+    res.render("index", { projectName: "Dots" });
+});
+
+module.exports = app;
