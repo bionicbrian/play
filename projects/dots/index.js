e99f20364e391d47eaf992eec3c2a13b86eb5ce1
Brian Moore <bmoore@book.com>
2013-12-26 08:42:38 -0500

    Move project views into views dir

--- projects/dots/index.js
+++ projects/dots/index.js
@@ -5,7 +5,7 @@ var stylus  = require("stylus");
 var express = require("express");
 var app     = express();
 
-app.set("views", __dirname);
+app.set("views", __dirname + "/views");
 app.use(stylus.middleware(__dirname + "/public"));
 app.use(express.static(path.join(__dirname, "public")));
 
